b9ab738a4498b0a5fc037a5f59e16628c882c919
Azrul <muhammadazrulliansyah@gmail.com>
2025-10-20 18:32:52 +0800

    change loop statement in main function

--- Rock-Paper-Scissor/rock-paper-scisor.c
+++ Rock-Paper-Scissor/rock-paper-scisor.c
@@ -7,16 +7,18 @@
 void game() {
 	int player_Choice;
 	int computer;
-	int min = 1;
-	int max = 3;
+	int min = 1, max = 3;
 
 	printf("Chose your optiion! \n1. Rock 2.Paper 3.Scissor\n>>");
-	scanf("%d", &player_Choice);
+	scanf(" %d", &player_Choice);
 	printf("Computer is Choosing.....\n");
 	computer = rand() % (max - min + 1) + min;
 	printf("Compter choose %d", computer);
 
-	if(player_Choice > computer || player_Choice == 1 && computer == 3) {
+	if(
+		(player_Choice == 1 && computer == 2) ||
+		(player_Choice == 2 && computer == 3) ||
+		(player_Choice == 3 && computer == 1)) {
 		printf("\n=== You Win! ===\n");
 	} else if (computer == player_Choice) {
 		printf("\n### DRAW ###\n");
@@ -30,28 +32,23 @@ void game() {
 
 int main() {
 	char answer;
+
+	srand(time(0));
+
 	printf("===== Welcome to Rock-Paper-Scissor Game! =====\n");
 	printf("Want to Play? Y/n\n>>");
-	scanf("%c", &answer);
+	scanf(" %c", &answer);
 		
-	if(answer == 'Y' || answer == 'y') {
-		int i = 0;
-		for (int i = 0; i < 1; ++i)
-		{
-			game();
+	//if(answer == 'Y' || answer == 'y') {
+	do{
+		game ();
+			
+		printf("Rematch? Y/n\n>>");
+		scanf(" %c", &answer);
 
-			while(1) {
-			    printf("Rematch? Y/n\n>>");
-			    scanf("%c", &answer);
-			    if (answer != 'n' || answer != 'N') {
-			    	game();
-			    }
-			}
-		}
-	} else {
-		printf("Thank you for playing!");
-		exit(0);
-	}
+	} while(answer != 'n' || answer != 'N');
+	//}
+	printf("Thank you for playing!");
 
 
 	return 0;
