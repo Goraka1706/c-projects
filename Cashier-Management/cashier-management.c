c396b431546cea7394f6ced338a95930094180f6
Azrul <muhammadazrulliansyah@gmail.com>
2025-10-24 15:48:59 +0800

    add cashier management

--- Cashier-Management/cashier-management.c
+++ Cashier-Management/cashier-management.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
 int main() {
-	char item, answer;
-	int itemCount;
+	char item, answer, exit;
+	int itemCount, totalItem;
 	double price, total;
-
+	price = 0.0;
 	printf("=== Cashier Management ===\n");
 
-	do{
-		printf("Item name: ");
-		scanf(" %s", &item);
-		printf("Amount: ");
-		scanf(" %d", &itemCount);
-		printf("Price: ");
-		scanf(" %lf", &price);
-		printf("=Done=\n=Again=");
-		scanf(" %s", &answer);
-		if (asnwer != "Again" || answer != "again") {
-			goto end;
+	printf("Enter program?\n");
+	printf("Press \'Enter/Return\' to enter program\nPress any key to exit\n");
+	printf("=Enter=   =Exit=\n");
+	//scanf(" %c", &answer);
+	answer = getchar();
+
+	if (answer == '\n')
+	{
+		//printf("Total Price: %lf.2 \n", price);
+		itemCount = 1;
+		for (int i = 0; i < itemCount; ++i)
+		{
+			printf("\nEnter item count: ");
+			scanf(" %d", &itemCount);
+			printf("\nItem name: ");
+			scanf(" %s", &item);
+			printf("\nPrice: ");
+			scanf("%lf", &price);
+
+			totalItem += itemCount;
+			total += price;
+
+			printf("\nTotal item: %d \nTotal price: %lf", totalItem, total);
+
 		}
-
-	} while(1);
-	end:
-
-
+		//printf("Success\n");
+	} else {
+		printf("Exit Success\n");
+	}
+	
 
 	return 0;
 }
