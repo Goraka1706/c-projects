aac6d2277966488155bc78b6a378cf9560e76f02
Azrul <muhammadazrulliansyah@gmail.com>
2025-10-30 11:59:35 +0800

    add rock-paper-scissor code version 2

---
+++ Rock-Paper-Scissor/rock-paper-scisor2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void game() {
+	int player_Choice;
+	int computer;
+	int min = 1, max = 3;
+
+	printf("Chose your optiion! \n1. Rock 2.Paper 3.Scissor\n>>");
+	scanf(" %d", &player_Choice);
+	printf("Computer is Choosing.....");
+	computer = rand() % (max - min + 1) + min;	
+	printf("\nComputer choose %d", computer);
+
+	if(
+		(player_Choice == 1 && computer == 2) ||
+		(player_Choice == 2 && computer == 3) ||
+		(player_Choice == 3 && computer == 1)) {
+		printf("\n=== You Win! ===\n");
+	} else if (player_Choice == computer) {
+		printf("\n### DRAW ###\n");
+	} else {
+		printf("\n--- Computer Win ---\n");
+	}
+	
+}
+
+
+
+int main() {
+	char answer;
+	int repeat = 1;
+	//start:
+	
+	while(repeat) {
+	    srand(time(0));
+
+		printf("===== Welcome to Rock-Paper-Scissor Game! =====\n");
+		printf("Want to Play? Y/n\n>>");
+		scanf(" %c", &answer);
+		
+		if(answer == 'Y' || answer == 'y') {
+			do{
+				game ();
+				
+				printf("Rematch? Y/n\n>>");
+				scanf(" %c", &answer);
+			} while(answer == 'Y' || answer == 'y');
+		//goto start;
+		} else {
+			printf("Thank you for playing!");
+			break;
+		}
+		switch(answer){
+		case 'n' | 'N':
+			continue;
+		}
+	}
+
+	return 0;
+}
