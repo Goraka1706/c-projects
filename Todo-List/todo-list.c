ed51afa184502b852e4733ec64228ccc4fc020e4
Azrul <muhammadazrulliansyah@gmail.com>
2025-10-31 16:28:37 +0800

    add todo list project file

---
+++ Todo-List/todo-list.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STRING_SIZE 1024
+
+
+
+void add() {
+	int add_list;
+	printf("\nEnter text:\n>>");
+	scanf("%s", &add_list);
+
+	FILE *list = fopen("TodoList.txt", "r");
+	if (list == NULL)
+	{
+		fopen("TodoList.txt", "w");
+	}
+	fprintf(add_list, "%s\n", TodoList.txt);
+
+	fclose(TodoList.txt);
+}
+
+void del() {
+	int delete_list;
+}
+
+
+int main() {
+	int answer, exit;
+
+	printf("===== Todo List =====\n");
+	printf("=== Presss number to select ===\n1. Add\n2. Delete\n3. Exit\n>>");
+	scanf("%s", &answer);
+
+	if(answer == 1) {
+		/* code */
+	}
+	else if(answer == 2) {
+		/* code */
+	}
+	else if (answer == 3)
+	{
+		printf("\nExit the program...");
+		exit(1);
+	}
+	else{
+		printf("\nWrong option! Please select the right one!\n");
+	}
+
+	return 0;
+}
