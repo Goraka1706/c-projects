78715216c3df792a1ea3fdbd10c95e3f6ad3321b
Azrul <muhammadazrulliansyah@gmail.com>
2025-11-16 15:09:42 +0800

    add library management project

---
+++ Library-Management/library_manage.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1024
+
+typedef struct {
+	char name[SIZE];
+	char author[SIZE];
+	char date[SIZE];
+	char price[SIZE];
+} Book;
+
+
+int main()
+{
+	return 0;
+}
